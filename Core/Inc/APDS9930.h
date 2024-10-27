90b3b457bc993613de973e7a5323a36eec6877be
morena1001 <josueflores15.jf@gmail.com>
2024-10-26 23:10:57 -0500

    JUST NEED to debug. Data not being read

---
+++ Core/Inc/APDS9930.h
@@ -0,0 +1,98 @@
+/*
+ * APDS9930.h
+ *
+ * Created on: October 25, 2024
+ * Author: Josue Flores
+ *
+ * Last modified: October 25, 2024
+ */
+
+#ifndef INC_APDS9930_H_
+#define INC_APDS9930_H_
+
+#include "stm32f3xx_hal.h" // NEEDED FOR I2C
+
+/*
+ * DEFINES
+ */
+
+#define APDS9930_I2C_ADDR		(0x39 << 1)
+#define APDS9930_ID_1			0x39
+#define APDS9930_ID_2			0x12
+
+/*
+ * REGISTERS p.18
+ */
+
+#define APDS9930_REG_ENABLE		0x00
+#define APDS9930_REG_ATIME		0x01
+#define APDS9930_REG_PTIME		0x02
+#define APDS9930_REG_WTIME		0x03
+#define APDS9930_REG_AILTL		0x04
+#define APDS9930_REG_AILTH		0x05
+#define APDS9930_REG_AIHTL		0x06
+#define APDS9930_REG_AIHTH		0x07
+#define APDS9930_REG_PILTL		0x08
+#define APDS9930_REG_PILTH		0x09
+#define APDS9930_REG_PIHTL		0x0A
+#define APDS9930_REG_PIHTH		0x0B
+#define APDS9930_REG_PERS		0x0C
+#define APDS9930_REG_CONFIG		0x0D
+#define APDS9930_REG_PPULSE		0x0E
+#define APDS9930_REG_CONTROL	0x0F
+#define APDS9930_REG_ID			0x12
+#define APDS9930_REG_STATUS		0x13
+#define APDS9930_REG_Ch0DATAL	0x14
+#define APDS9930_REG_Ch0DATAH	0x15
+#define APDS9930_REG_Ch1DATAL	0x16
+#define APDS9930_REG_Ch1DATAH	0x17
+#define APDS9930_REG_PDATAL		0x18
+#define APDS9930_REG_PDATAH		0x19
+#define APDS9930_REG_POFFSET	0x0E
+
+/*
+ * LUX COEFFICIENTS p.9
+ */
+
+#define DF		52
+#define GA		0.49
+#define B		1.862
+#define C		0.746
+#define D		1.291
+
+/*
+ * SENSOR STRUCT
+ */
+
+typedef struct {
+	I2C_HandleTypeDef* i2c_handle;
+
+	// processed lux and proximity (in mm) data
+	float lux;
+	float prox;
+} APDS9930_t;
+
+/*
+ * INITIALIZATION
+ */
+
+// Outputs possible error codes
+uint8_t APDS9930_Init (APDS9930_t* device, I2C_HandleTypeDef* i2c_handle);
+
+/*
+ * DATA AQUISITION
+ */
+
+HAL_StatusTypeDef APDS9930_ReadLux (APDS9930_t* device);
+HAL_StatusTypeDef APDS9930_ReadProximity (APDS9930_t* device);
+
+/*
+ * LOW LEVEL FUNCTIONS
+ */
+
+HAL_StatusTypeDef APDS9930_ReadRegister (APDS9930_t* device, uint8_t reg, uint8_t* data);
+HAL_StatusTypeDef APDS9930_ReadRegisters (APDS9930_t* device, uint8_t reg, uint8_t* data, uint8_t length);
+
+HAL_StatusTypeDef APDS9930_WriteRegister (APDS9930_t* device, uint8_t reg, uint8_t* data);
+
+#endif /* INC_APDS9930_H_ */
---
+++ Core/Src/APDS9930.c
@@ -0,0 +1,185 @@
+/*
+ * APDS9930.c
+ *
+ * Created on: October 25, 2024
+ * Author: Josue Flores
+ *
+ * Last modified: October 25, 2024
+ */
+
+#include <stdarg.h>
+
+#include "APDS9930.h"
+
+// PRIVATE VARIABLES
+float ALSIT;
+uint8_t AGAIN;
+
+uint16_t Ch0_data;
+uint16_t Ch1_data;
+
+float IAC1;
+float IAC2;
+
+float IAC;
+float LPC;
+
+// PRIVATE FUNCTION PROTOTYPES
+float Max (int num, ...);
+uint8_t Pow (uint8_t base, uint8_t exp);
+
+/*
+ * INITIALIZATION
+ */
+
+uint8_t APDS9930_Init (APDS9930_t* device, I2C_HandleTypeDef* i2c_handle) {
+	// Set struct parameters
+	device->i2c_handle = i2c_handle;
+
+	device->lux = 0.0f;
+	device->prox = 0.0f;
+
+	Ch0_data = 0;
+	Ch1_data = 0;
+
+	IAC1 = 0.0f;
+	IAC2 = 0.0f;
+
+	IAC = 0.0f;
+	LPC = 0.0f;
+
+	// Store number of transaction errors ( to be returned at the end of the function )
+	uint8_t err_num = 0;
+	HAL_StatusTypeDef status;
+
+	// Check ID register (p.23)
+	uint8_t reg_data;
+	status = APDS9930_ReadRegister (device, APDS9930_REG_ID, &reg_data);
+	err_num += (status != HAL_OK);
+
+	if (reg_data != APDS9930_ID_1 && reg_data != APDS9930_ID_2)	return 255;
+
+	// ENABLE : WEN, PEN, AEN, and PON are enabled. FOR THE MOMENT PIEN and AIEN are disaled;
+	reg_data = 0x0F;
+	// reg_data = 0x3F;
+	status = APDS9930_WriteRegister (device, APDS9930_REG_ENABLE, &reg_data);
+	err_num += (status != HAL_OK);
+
+	// ALS Timing Register : 1 cycle (2.73ms)
+	reg_data = 0xff;
+	status = APDS9930_WriteRegister (device, APDS9930_REG_ATIME, &reg_data);
+	err_num += (status != HAL_OK);
+
+	ALSIT = 2.73 * (float) (256 - reg_data);
+
+	// Proximity Time Control Register : 1 cycle (2.73ms)
+	reg_data = 0xff;
+	status = APDS9930_WriteRegister (device, APDS9930_REG_PTIME, &reg_data);
+	err_num += (status != HAL_OK);
+
+	// Wait Time Register : 1 cycle (2.73ms)
+	reg_data = 0xff;
+	status = APDS9930_WriteRegister (device, APDS9930_REG_WTIME, &reg_data);
+	err_num += (status != HAL_OK);
+
+	// ALS INTERRUPT THRESHOLD REGISTERS
+	// PROXIMITY INTERRUPT THRESHOLD REGISTERS
+	// PERSISTENCE REGISTER
+	// NEED TO BE CONFIGURED FOR INTERRUPTS
+
+	// Configuration Register : AGL, WLONG, and PDL are not asserted
+	reg_data = 0x00;
+	status = APDS9930_WriteRegister (device, APDS9930_REG_CONFIG, &reg_data);
+	err_num += (status != HAL_OK);
+
+	AGAIN = Pow (2, reg_data >> 2);
+
+	// Proximity Pulse Count Register : 8 pulses (recommended p.22)
+	reg_data = 0x08;
+	status = APDS9930_WriteRegister (device, APDS9930_REG_PPULSE, &reg_data);
+	err_num += (status != HAL_OK);
+
+	// Control Register : 100mA LED strength, Proximity uses Ch1 diode, 1x Proximity gain, 1x ALS gain
+	reg_data = 0x20;
+	status = APDS9930_WriteRegister (device, APDS9930_REG_CONTROL, &reg_data);
+	err_num += (status != HAL_OK);
+
+	return err_num;
+}
+
+/*
+ * DATA AQUISITION
+ */
+
+HAL_StatusTypeDef APDS9930_ReadLux (APDS9930_t* device) {
+	uint8_t raw_data[4];
+	HAL_StatusTypeDef status;
+
+	status = APDS9930_ReadRegisters (device, APDS9930_REG_Ch0DATAL, raw_data, 4);
+	Ch0_data = 256 * raw_data[1] + raw_data[0];
+	Ch1_data = 256 * raw_data[3] + raw_data[2];
+
+	IAC1 = (float) Ch0_data - B * (float ) Ch1_data;
+	IAC2 = C * (float) Ch0_data - D * (float) Ch1_data;
+
+	IAC = Max (2, IAC1, IAC2);
+	LPC = GA * DF / (ALSIT * (float) AGAIN);
+
+	device->lux = IAC * LPC;
+
+	return status;
+}
+
+HAL_StatusTypeDef APDS9930_ReadProximity (APDS9930_t* device) {
+	uint8_t raw_data[2];
+	HAL_StatusTypeDef status;
+
+	status = APDS9930_ReadRegisters (device, APDS9930_REG_PDATAL, raw_data, 2);
+	device->prox = 256 * raw_data[1] + raw_data[0];
+
+	return status;
+}
+
+/*
+ * LOW-LEVEL FUNCTIONS
+ */
+
+HAL_StatusTypeDef APDS9930_ReadRegister (APDS9930_t* device, uint8_t reg, uint8_t* data) {
+	return HAL_I2C_Mem_Read(device->i2c_handle, APDS9930_I2C_ADDR, reg, I2C_MEMADD_SIZE_8BIT, data, 1, HAL_MAX_DELAY);
+}
+
+HAL_StatusTypeDef APDS9930_ReadRegisters (APDS9930_t* device, uint8_t reg, uint8_t* data, uint8_t length) {
+	return HAL_I2C_Mem_Read(device->i2c_handle, APDS9930_I2C_ADDR, reg, I2C_MEMADD_SIZE_8BIT, data, length, HAL_MAX_DELAY);
+}
+
+HAL_StatusTypeDef APDS9930_WriteRegister (APDS9930_t* device, uint8_t reg, uint8_t* data) {
+	return HAL_I2C_Mem_Write(device->i2c_handle, APDS9930_I2C_ADDR, reg, I2C_MEMADD_SIZE_8BIT, data, 1, HAL_MAX_DELAY);
+}
+
+/*
+ * PRIVATE FUNCTIONS
+ */
+
+float Max (int num, ...) {
+	float max = 0.0f;
+	float temp = 0.0f;
+	va_list vl;
+	va_start (vl, num);
+
+	for (int i = 0; i < num; i++) {
+		temp = (float) va_arg (vl, double);
+		max = temp > max ? temp : max;
+	}
+
+	return max;
+}
+
+uint8_t Pow (uint8_t base, uint8_t exp) {
+	uint8_t answer = 1;
+
+	for (int i = 0; i < exp; i++) {
+		answer *= base;
+	}
+
+	return answer;
+}
